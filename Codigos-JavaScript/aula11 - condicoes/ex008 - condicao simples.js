030800280fa249ed26ed8d75dce557082a567cf7
Francisco Cleiton <82819686+franciscocleiton@users.noreply.github.com>
2021-12-27 17:09:18 -0300

    Condicoes

---
+++ Codigos-JavaScript/aula11 - condicoes/ex008 - condicao simples.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu caorro é ${vel}km/h`)
+
+if(vel > 60){ // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log('Digita sempre usando cinto de segurança')
